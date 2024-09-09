4400bf4e45bf4c37d63d5cfd8137290aa7e1535f
Ricardo <ricardo85x@gmail.com>
2024-09-09 10:08:26 -0300

    feat: heap usage

---
+++ 07-heaps/00-sample.py
@@ -0,0 +1,30 @@
+# In Python, we will use the heapq module
+# Note: heapq only implements min heaps
+from heapq import *
+
+# Declaration: heapq does not give you a heap data structure.
+# You just use a normal list, and heapq provides you with
+# methods that can be used on this list to perform heap operations
+heap = []
+
+# Add to heap
+heappush(heap, 1)
+heappush(heap, 2)
+heappush(heap, 3)
+
+# Check minimum element
+heap[0] # 1
+
+# Pop minimum element
+heappop(heap) # 1
+
+# Get size
+len(heap) # 2
+
+# Bonus: convert a list to a heap in linear time
+nums = [43, 2, 13, 634, 120]
+heapify(nums)
+
+# Now, you can use heappush and heappop on nums
+# and nums[0] will always be the minimum element
+print(nums) # [2, 43, 13, 634, 120]
