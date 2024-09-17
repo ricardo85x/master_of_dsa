2d75c75e608c73e56b49ddb5615e926902fcbf49
Ricardo <ricardo85x@gmail.com>
2024-09-17 09:03:52 -0300

    feat: sliding window - find length

---
+++ 02-arrays-and-strings/sliding-window/find-length.py
@@ -0,0 +1,22 @@
+# Find length
+
+# Given an array of positive integers 
+#   nums and an integer k, 
+# find the length of the longest subarray 
+#   whose sum is less than or equal to k. 
+
+def find_length(nums: list[int], k: int) -> int:
+    left = curr = ans = 0
+    
+    for right in range(len(nums)):
+        curr += nums[right]
+        
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+            
+        ans = max(ans, right - left + 1)
+    
+    return ans
+
+print(find_length([3,1,2,7,4,2,1,1,5], 8))
