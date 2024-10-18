fe12be72c7f7383e0744483354fce95709f0faa1
Ricardo <ricardo85x@gmail.com>
2024-10-18 08:42:41 -0300

    feat: fix and add test to permute backtracking

--- 10-backtracking/01-permute.py
+++ 10-backtracking/01-permute.py
@@ -19,14 +19,40 @@ def permute(nums: list[int]) -> list[list[int]]:
         if len(curr) == len(nums):
             ans.append(curr[:])
             return
-        
         for num in nums:
-            if num not in ans:
+            if num not in curr:
                 curr.append(num)
                 backtrack(curr)
                 curr.pop()
+                
     ans = []
     backtrack([])
     return ans
 
-print(permute([1,2]))
+import unittest
+
+class TestDSA(unittest.TestCase):
+    
+    def test_example_1(self):
+        nums = [1, 2]
+        expected_output = [
+            [1,2],
+            [2,1]
+        ]
+        self.assertEqual(permute(nums), expected_output)
+    
+    def test_example_2(self):
+        nums = [1, 2, 3]
+        expected_output = [
+            [1,2,3],
+            [1,3,2],
+            [2,1,3],
+            [2,3,1],
+            [3,1,2],
+            [3,2,1]
+        ]
+        self.assertEqual(permute(nums), expected_output)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
